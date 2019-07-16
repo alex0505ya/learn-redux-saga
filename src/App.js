f630d2ad5dadc1ac43d240f10c70329aff9da3d2
davidniem <billiontest168@gmail.com>
2019-07-16 11:59:38 +0800

    'init-commit'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from 'redux'
+import Counter from './Counter'
+import reducer from './reducers'
+const store = createStore(reducer)
+
+const action = type => store.dispatch({ type })
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')} /> )
 }
-
+store.subscribe(App)
 export default App;
